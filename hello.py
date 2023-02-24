dc829626f7221f9e9d037019a91a925f9ab0ed6a
Facundo <facudelvalle38@gmail.com>
2023-02-24 01:35:52 -0300

    primer commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hola_mundo():
+    return "<p>Hola mundooo</p>"
+
+@app.route('/usuario/<usernamee>')
+def mostrar_usuario(usernamee):
+    return f"<p>Usuario: {escape(usernamee)}!</p>"
+
+@app.route('/post/<int:post_id>')
+
+def mostrar_post(post_id):
+    return f"<p>Post id: {escape(post_id)}</p>"
